a1db950f0705523857f86599d27583b3a8988163
a.nikfar <a.nikfar@DPK-158>
2023-12-31 10:41:51 +0330

    Add project files.

---
+++ FinalSophieTravelManagment.Domain/ValueObject/TravelerCheckListId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSophieTravelManagment.Domain.ValueObject
+{
+    internal record TravelerCheckListId
+    {
+        public Guid Value { get; }
+        public  TravelerCheckListId(Guid value)
+        { 
+              if (value ==Guid.Empty)
+              {
+                  throw new ArgumentException();
+              }
+                  Value = value;
+        }
+
+        public static implicit operator Guid (TravelerCheckListId id) => id.Value;
+        public static implicit operator TravelerCheckListId(Guid id) => new(id);
+    }
+}
