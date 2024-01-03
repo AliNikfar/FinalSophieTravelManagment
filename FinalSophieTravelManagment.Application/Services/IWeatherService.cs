5d5e0ffdb814a43bd3b77fb2e55671b80ef4c10b
a.nikfarDPK <ali.nikfar2000@gmail.com>
2024-01-03 15:55:13 +0330

    Add Services And other..

---
+++ FinalSophieTravelManagment.Application/Services/IWeatherService.cs
@@ -0,0 +1,15 @@
+﻿using FinalSophieTravelManagment.Application.DTO.External;
+using FinalSophieTravelManagment.Domain.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSophieTravelManagment.Application.Services
+{
+    public interface IWeatherService 
+    {
+        Task<WeatherDTO> GetWeatherAsync(Destination localization);
+    }
+}
