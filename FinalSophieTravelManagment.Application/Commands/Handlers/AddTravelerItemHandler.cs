4fadec87959009a15788239b8d102221b563576d
a.nikfarDPK <ali.nikfar2000@gmail.com>
2024-01-06 19:17:34 +0330

    Add Handler

---
+++ FinalSophieTravelManagment.Application/Commands/Handlers/AddTravelerItemHandler.cs
@@ -0,0 +1,32 @@
+﻿using FinalSophieTravelManagment.Domain.Repositories;
+using FinalSophieTravelManagment.Shared.Abstractions.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSophieTravelManagment.Application.Commands.Handlers
+{
+    internal sealed class AddTravelerItemHandler : ICommandHandler<AddTravelerItem>
+    {
+        private readonly ITravelereCheckListRepository _repository;
+
+        public AddTravelerItemHandler(ITravelereCheckListRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task HandleAsync(AddTravelerItem command)
+        {
+            var travelerCheckingList = await _repository.GetAsync(command.TravelerCheckListId);
+
+            if(travelerCheckingList is null)
+            {
+                throw new TravelerChekListNotFound(command.TravelerCheckListId);
+            }
+            var travelerItem = new AddTravelerItem(command.Name, command.Quantity);
+            travelerCheckingList.AddItem(travelerItem);
+        }
+    }
+}
---
+++ FinalSophieTravelManagment.Application/Exceptions/TravelerChekListNotFound.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace FinalSophieTravelManagment.Application.Exceptions
+{
+    [Serializable]
+    internal class TravelerChekListNotFound : Exception
+    {
+        public TravelerChekListNotFound(string? message) : base(message)
+        {
+        }
+
+    }
+}
